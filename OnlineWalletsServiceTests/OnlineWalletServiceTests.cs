e389ebb0d7c6bb3417d00d65d740e940642ed60e
Robin Laszlo <laszlorobin92@gmail.com>
2024-11-22 17:58:33 +0100

    Unit test project setup with the neccessary extensions. Also added 2 unit tests for the GetBalanceAsync  method to test out the nuget and the references are working fine.

---
+++ OnlineWalletsServiceTests/OnlineWalletServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Betsson.OnlineWallets.Data.Models;
+using Betsson.OnlineWallets.Data.Repositories;
+using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Services;
+using FluentAssertions;
+using Moq;
+
+namespace Betsson.OnlineWallets.UnitTests
+{
+    public class OnlineWalletServiceTests
+    {
+        private readonly Mock<IOnlineWalletRepository> _mockRepository;
+        private readonly OnlineWalletService _service;
+
+        public OnlineWalletServiceTests()
+        {
+            _mockRepository = new Mock<IOnlineWalletRepository>();
+            _service = new OnlineWalletService(_mockRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetBalanceAsync_ShouldReturnBalance()
+        {
+            // Arrange
+            var onlineWalletEntry = new OnlineWalletEntry { BalanceBefore = 100, Amount = 50 };
+            _mockRepository.Setup(repo => repo.GetLastOnlineWalletEntryAsync())
+                .ReturnsAsync(onlineWalletEntry);
+
+            // Act
+            var result = await _service.GetBalanceAsync();
+
+            // Assert
+            result.Amount.Should().Be(150);
+        }
+
+        [Fact]
+        public async Task GetBalanceAsync_ShouldReturnCorrectBalance_WhenTransactionExists()
+        {
+            // Arrange
+            var lastEntry = new OnlineWalletEntry
+            {
+                Amount = 50,
+                BalanceBefore = 100
+            };
+
+            _mockRepository
+                .Setup(repo => repo.GetLastOnlineWalletEntryAsync())
+                .ReturnsAsync(lastEntry);
+
+            // Act
+            Balance result = await _service.GetBalanceAsync();
+
+            // Assert
+            result.Amount.Should().Be(150);
+            _mockRepository.Verify(repo => repo.GetLastOnlineWalletEntryAsync(), Times.Once);
+        }
+
+    }
+}
