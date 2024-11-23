3e59deabd8396ca0dbd45f7e7f0e6e3097da742d
Robin Laszlo <laszlorobin92@gmail.com>
2024-11-23 20:45:05 +0100

    Created project for the API tests and added the test for the Balance endpoint to make sure every nuget and reference is working well.

---
+++ Betsson.OnlineWalletsController.ApiTests/OnlineWalletControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Betsson.OnlineWallets.Data.Models;
+using Betsson.OnlineWallets.Data.Repositories;
+using Betsson.OnlineWallets.Models;
+using Betsson.OnlineWallets.Web;
+using Betsson.OnlineWallets.Web.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Net.Http.Json;
+using System.Net;
+
+namespace Betsson.OnlineWalletsController.ApiTests
+{
+    public class OnlineWalletControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public OnlineWalletControllerTests()
+        {
+            _factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var repositoryMock = new Mock<IOnlineWalletRepository>();
+
+                        repositoryMock.Setup(r => r.GetLastOnlineWalletEntryAsync())
+                            .ReturnsAsync(new OnlineWalletEntry
+                            {
+                                BalanceBefore = 100,
+                                Amount = 50
+                            });
+
+                        repositoryMock.Setup(r => r.InsertOnlineWalletEntryAsync(It.IsAny<OnlineWalletEntry>()))
+                            .Returns(Task.CompletedTask);
+
+                        services.AddSingleton(repositoryMock.Object);
+
+                        
+                        var mapperConfig = new MapperConfiguration(cfg =>
+                        {
+                            cfg.CreateMap<Balance, BalanceResponse>();
+                            cfg.CreateMap<DepositRequest, Deposit>();
+                            cfg.CreateMap<WithdrawalRequest, Withdrawal>();
+                        });
+                        services.AddSingleton(mapperConfig.CreateMapper());
+                    });
+                });
+        }
+
+        [Fact]
+        public async Task Balance_Should_Return_Correct_Amount()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/OnlineWallet/Balance");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var balanceResponse = await response.Content.ReadFromJsonAsync<BalanceResponse>();
+
+            // Assert
+            balanceResponse.Should().NotBeNull();
+            balanceResponse.Amount.Should().Be(150);
+        }
+    }
+}
